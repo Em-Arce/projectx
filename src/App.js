a4f58eca6fb6075b348feab9a079ea8fbb44449e
Em-Arce <emjenarce@hotmail.com>
2022-12-13 14:25:52 +0800

    Semicolons

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from "react"
+import React from "react";
 import './App.css';
-import PaginationTable from './components/AllCoinsTable';
+import AllCoinsTable from './components/AllCoinsTable';
 
 function App() {
     return (
@@ -8,9 +8,9 @@ function App() {
             <nav className="relative flex justify-center align-center max-w-md mx-auto bg-white overflow-hidden md:max-w-2xl mt-2 mb-2">
               <h4 className="text-xl sm: text-sm font-semibold leading-normal mt-2 mb-2">All Cryptocurrencies</h4>
             </nav>
-            <PaginationTable />
+            <AllCoinsTable />
         </div>
     )
-}
+};
 
 export default App;
